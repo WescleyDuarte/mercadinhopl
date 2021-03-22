6744e41f779fa9952a57bc2b259aa7eb8b5bf604
Riccely <80008023+Riccely@users.noreply.github.com>
2021-03-22 19:29:22 -0300

    Update Usuario.java

--- src/br/edu/ufersa/model/Usuario.java
+++ src/br/edu/ufersa/model/Usuario.java
@@ -3,13 +3,13 @@ class Usuario{
 	String login;
 	String senha;
 
-	public void cadastrar(Usuario usuarior){
+	public void cadastrar(Usuario usuario){
 		//Cadastrar os dados do novo usuario para enviar ao BD.
 	}
 	public void alterar(Usuario usuario){
 		login = usuario.login;
 		senha = usuario.senha;
-		//salvar alterações no BD
+		//salvar alteraÃ§Ãµes no BD
 	}
 	public void deletar(Usuario usuario){
 		//Deletar do BD
