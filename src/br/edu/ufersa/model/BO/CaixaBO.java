eb33224f26e1ac69932ddfbe25c5ce13c792fedb
Riccely <80008023+Riccely@users.noreply.github.com>
2021-04-15 01:06:47 -0300

    new gerente e mudanças caixa

--- src/br/edu/ufersa/model/BO/CaixaBO.java
+++ src/br/edu/ufersa/model/BO/CaixaBO.java
@@ -9,18 +9,18 @@ import br.edu.ufersa.model.VO.*;
 public class CaixaBO {
 
 	static Scanner scanner = new Scanner(System.in);
-	CaixaDAO caixaDAO = new CaixaDAO();
-	CaixaVO caixa = new CaixaVO();
-	ArrayList<CaixaVO> caixas = new ArrayList<CaixaVO>();
-	int controlador = 0;
-	UsuarioDAO usuarioDAO= new UsuarioDAO();
-
+	private CaixaDAO caixaDAO = new CaixaDAO();
+	private CaixaVO caixa = new CaixaVO();
+	private ArrayList<CaixaVO> caixas = new ArrayList<CaixaVO>();
+	private int controlador = 0;
+	private UsuarioDAO usuarioDAO= new UsuarioDAO();
+	private GerenteDAO gerenteDAO = new GerenteDAO();
 
 	//precisa ter uma condição onde ele for cadastrar se a opção for caixa então entra aqui
 	//passando o id do usuario
     public void cadastrarCaixa(int id){
 		
-		if(usuarioDAO.buscarUsuarioPorID(id)!=null){
+		if(usuarioDAO.buscarUsuarioPorID(id)!=null && gerenteDAO.buscarGerente(id)==null && caixaDAO.buscarCaixa(id)==null){
 
 			controlador=0;
 			while(controlador==0){
@@ -56,27 +56,33 @@ public class CaixaBO {
 		if(caixaDAO.buscarCaixa(id)!=null){
 			System.out.println(caixa.toString());
 
-			System.out.println("digite o novo Salario para o Caixa:");
-			caixa.setSalario(scanner.nextFloat());
+			controlador=0;
+			while(controlador==0){
+				System.out.println("digite o novo Salario para o Caixa:");
+				caixa.setSalario(scanner.nextFloat());
 
-			if(caixa.getSalario()>=1100){
+				if(caixa.getSalario()>=1100){
 
-					caixa.setNome(usuarioDAO.buscarUsuarioPorID(id).getNome());
-					caixa.setSexo(usuarioDAO.buscarUsuarioPorID(id).getSexo());
-					caixa.setIdade(usuarioDAO.buscarUsuarioPorID(id).getIdade());
-					caixa.setId(id);
-					caixa.setLogin(usuarioDAO.buscarUsuarioPorID(id).getLogin());
-					caixa.setSenha(usuarioDAO.buscarUsuarioPorID(id).getSenha());
+						caixa.setNome(usuarioDAO.buscarUsuarioPorID(id).getNome());
+						caixa.setSexo(usuarioDAO.buscarUsuarioPorID(id).getSexo());
+						caixa.setIdade(usuarioDAO.buscarUsuarioPorID(id).getIdade());
+						caixa.setId(id);
+						caixa.setLogin(usuarioDAO.buscarUsuarioPorID(id).getLogin());
+						caixa.setSenha(usuarioDAO.buscarUsuarioPorID(id).getSenha());
 
 
-				caixas = caixaDAO.buscaExcludente(id);
+					caixas = caixaDAO.buscaExcludente(id);
 
-				CaixaDAO.cadastrarAuxiliar(caixa);
+					CaixaDAO.cadastrarAuxiliar(caixa);
+					controlador++;
+					for(int i=0;caixas.get(i)!=null;i++){
 
-				for(int i=0;caixas.get(i)!=null;i++){
-
-					caixaDAO.cadastrar(caixas.get(i));
+						caixaDAO.cadastrar(caixas.get(i));
+					}
+				}else{
+					System.out.println("\nSalario invalido");
 				}
+
 			}
 		}else{
 			System.out.println("\n não foi possivil alterar o valor do salario do caixa tente novamente (valor>1100)");
--- src/br/edu/ufersa/model/BO/GerenteBO.java
+++ src/br/edu/ufersa/model/BO/GerenteBO.java
@@ -5,99 +5,93 @@ import br.edu.ufersa.model.VO.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import br.edu.ufersa.model.DAO.CaixaDAO;
 import br.edu.ufersa.model.DAO.GerenteDAO;
+import br.edu.ufersa.model.DAO.UsuarioDAO;
 
 public class GerenteBO {
 
 	static Scanner scanner = new Scanner(System.in);
-	GerenteVO gerente = new GerenteVO();
-	GerenteDAO gerenteDAO = new GerenteDAO();
-	ArrayList<GerenteVO> gerentes = new ArrayList<GerenteVO>();
-    
-    public void cadastrarGerente() {
+	private GerenteVO gerente = new GerenteVO();
+	private	GerenteDAO gerenteDAO = new GerenteDAO();
+	private ArrayList<GerenteVO> gerentes = new ArrayList<GerenteVO>();
+    private int controlador;
+	private UsuarioDAO usuarioDAO = new UsuarioDAO();
+	private CaixaDAO caixaDAO = new CaixaDAO();
 
-		GerenteVO gerenteAux = new GerenteVO();
-		
-		int controlador=0;
-		while(controlador ==0){
-			System.out.println("\nEntre com o nome do gerente: ");
-			gerenteAux.setNome(scanner.next());
-			if(gerenteAux.getNome() != null && gerenteAux.getNome()!=""){
-				gerente.setNome(gerenteAux.getNome());
-				controlador ++;
-			}else{
-				System.out.println("\nNome invalido, digite novamete!");
-			}
 
-			while(controlador==1){
-				System.out.println("\nEntre com a idade do gerente: ");
-				gerenteAux.setIdade(scanner.nextInt());
-				if(gerenteAux.getIdade()>0){
-					gerente.setIdade(gerenteAux.getIdade());
+
+    public void cadastrarGerente(int id) { //melhorar o tratamento futuramento com as outras opções de polimofismo
+
+		if(usuarioDAO.buscarUsuarioPorID(id)!=null && gerenteDAO.buscarGerente(id)==null && caixaDAO.buscarCaixa(id)==null ){
+			
+			controlador=0;
+			while(controlador==0){
+
+				System.out.println("\nDigite o CPF do caixa");
+				gerente.setCpf(scanner.nextInt());
+
+				if( gerente.getCpf()> 10000000000L && gerente.getCpf() < 99999999999L){
 					controlador++;
-				}else{
-					System.out.println("\nIdade invalida, digite novamente pfv!");
-				}
+					
+					gerente.setNome(usuarioDAO.buscarUsuarioPorID(id).getNome());
+					gerente.setSexo(usuarioDAO.buscarUsuarioPorID(id).getSexo());
+					gerente.setIdade(usuarioDAO.buscarUsuarioPorID(id).getIdade());
+					gerente.setId(id);
+					gerente.setLogin(usuarioDAO.buscarUsuarioPorID(id).getLogin());
+					gerente.setSenha(usuarioDAO.buscarUsuarioPorID(id).getSenha());
+					
+					
+					gerenteDAO.cadastrar(gerente);
 
-				while(controlador==2){
 
-					System.out.println("\nEntre com o sexo do gerente: ");
-					gerenteAux.setSexo(scanner.next());
-					if(gerenteAux.getSexo().equalsIgnoreCase("Masculino") || gerenteAux.getSexo().equalsIgnoreCase("Feminino")){
-						gerente.setSexo(gerenteAux.getSexo());
-						controlador++;
-					}else{
-						System.out.println("\nSexo invalido, digite novamente!!");
-					}
-
-					while(controlador ==3){
-						System.out.println("\nEntre com o CPF do gerente: "); // colocar a função valida cpf
-						gerenteAux.setCpf(scanner.nextLong());
-						
-						if(gerenteDAO.validarGCPF(gerenteAux.getCpf())!= null && gerenteAux.getCpf() > 10000000000L && gerenteAux.getCpf() < 99999999999L){
-							gerente.setCpf(gerenteAux.getCpf());
-							gerenteDAO.cadastrar(gerente);
-							controlador++;
-						}
-							
-					}
 				}
 			}
 		}
+			
 	}
 
 
-	public void alterar(String nomeDoGerente){
-		GerenteVO gerenteParaAlterar = new GerenteVO();
-
-		GerenteDAO buscador = new GerenteDAO();
-
-		gerenteParaAlterar = buscador.buscarGerente(nomeDoGerente);
-
-		// fazer o busca do Gerente
-		System.out.println(gerenteParaAlterar.toString());
+	public void alterar(int id){
 		
-		System.out.println("\nDigite o novo Nome para o Gerente: ");
-		gerenteParaAlterar.setNome(scanner.next());
-		System.out.println("\nDigite a nova Idade para o Gerente: ");
-		gerenteParaAlterar.setIdade(scanner.nextInt());
-		System.out.println("\nDigite o novo Sexo do Gerente: ");
-		gerenteParaAlterar.setSexo(scanner.next());
-		System.out.println("\nDigite o novo CPF do Gerente: ");
-		gerenteParaAlterar.setCpf(scanner.nextInt());
+		if(gerenteDAO.buscarGerente(id)!=null){
 
-		gerentes = gerenteDAO.buscaExcludente(nomeDoGerente);
+			gerente = gerenteDAO.buscarGerente(id);
 
-		GerenteDAO.cadastrarAuxiliar(gerenteParaAlterar);
+			System.out.println(gerente.toString());
+			controlador=0;
+			while(controlador==0){
+				System.out.println("\nDigite o novo CPF do Gerente: ");
+				gerente.setCpf(scanner.nextInt());
 
-		for(int i=0; gerentes.get(i)!=null;i++){
+				if( gerente.getCpf()> 10000000000L && gerente.getCpf() < 99999999999L){
 
-			gerenteDAO.cadastrar(gerentes.get(i));
+					gerente.setNome(usuarioDAO.buscarUsuarioPorID(id).getNome());
+					gerente.setSexo(usuarioDAO.buscarUsuarioPorID(id).getSexo());
+					gerente.setIdade(usuarioDAO.buscarUsuarioPorID(id).getIdade());
+					gerente.setId(id);
+					gerente.setLogin(usuarioDAO.buscarUsuarioPorID(id).getLogin());
+					gerente.setSenha(usuarioDAO.buscarUsuarioPorID(id).getSenha());
+
+					gerentes = gerenteDAO.buscaExcludente(id);
+
+					GerenteDAO.cadastrarAuxiliar(gerente);
+					controlador++;
+					for(int i=0; gerentes.get(i)!=null;i++){
+
+						gerenteDAO.cadastrar(gerentes.get(i));
+					}
+				}else{
+					System.out.println("\n Cpf invalido");
+				}
+			}
+		}else{
+			System.out.println("\n não foi possivel alterar ou gerente não existe");
 		}
 	}
 
-	public void deletar(String nomeDoGerente){
-		gerentes = gerenteDAO.buscaExcludente(nomeDoGerente);
+	public void deletar(int id){
+		gerentes = gerenteDAO.buscaExcludente(id);
 
 		
 		for(int i= 0;gerentes.get(i)!=null;i++){
