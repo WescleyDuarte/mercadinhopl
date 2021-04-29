3463e8b14d69e2640a84519125233f9063bb15ec
Riccely Guimarães <80008023+Riccely@users.noreply.github.com>
2021-04-28 23:07:31 -0300

    Add files via upload

--- src/br/edu/ufersa/model/BO/CaixaBO.java
+++ src/br/edu/ufersa/model/BO/CaixaBO.java
@@ -1,68 +1,204 @@
-package mercadinhopl.model.BO;
+package br.edu.ufersa.model.BO;
 
-import java.util.ArrayList;
+import br.edu.ufersa.model.DAO.*;
+import br.edu.ufersa.model.VO.*;
+
 import java.util.Scanner;
+//import java.util.*;
+import java.util.ArrayList;
+public class CaixaBO extends UsuarioBO{
 
-import mercadinhopl.model.DAO.CaixaDAO;
-import mercadinhopl.model.VO.CaixaVO;
+    static Scanner scanner = new Scanner(System.in);
+	private CaixaDAO caixaDAO = new CaixaDAO();
+	private CaixaVO caixa = new CaixaVO();
+	private ArrayList<CaixaVO> caixas = new ArrayList<CaixaVO>();
+	private int controlador = 0;
+	//private UsuarioDAO usuarioDAO= new UsuarioDAO();
+	private GerenteDAO gerenteDAO = new GerenteDAO();
 
-public class CaixaBO {
 
-	static Scanner scanner = new Scanner(System.in);
-	CaixaDAO caixaDAO = new CaixaDAO();
-	CaixaVO caixa = new CaixaVO();
-	ArrayList<CaixaVO> caixas = new ArrayList<CaixaVO>();
+    @Override
+    public void cargo(){
+        //Aqui teremos condição para relaização de alteração na nota( ou outra opção mais detalhada) // precisamos da interface grafica  para utilizar melhor
+    }
 
+
+    
+	//precisa ter uma condiÃ§Ã£o onde ele for cadastrar se a opÃ§Ã£o for caixa entÃ£o entra aqui
+	//passando o id do usuario
     public void cadastrarCaixa(){
-		
-		System.out.println("Entre com o nome do caixa: ");
-		caixa.setNome(scanner.next());;
-		System.out.println("Entre com a idade do caixa: ");
-		caixa.setIdade(scanner.nextInt());
-		System.out.println("Entre com o sexo do caixa: ");
-		caixa.setSexo(scanner.next());
-		System.out.println("Entre com o salario do caixa: ");
-		caixa.setSalario(scanner.nextFloat());
+	
+			controlador=0;
+			while(controlador!=7){
 
-		caixaDAO.cadastrar(caixa);
-		caixaDAO.listar();
+				if(controlador==0){
+					System.out.println("\nDigite o Salario do Caixa");
+					caixa.setSalario(scanner.nextFloat());
+
+					if( caixa.getSalario()> 1100 && caixa.getSalario()!=0){
+						controlador++;
+					}
+
+				if(controlador==1){
+					System.out.println("\nDigite o Nome do caixa");
+					caixa.setNome(scanner.next());
+				}
+
+					if(caixa.getNome() != null && caixa.getNome()!=""){
+					controlador ++;
+					}
+
+				if(controlador==2){
+						System.out.println("\nDigite o Sexo do caixa");
+						caixa.setSexo(scanner.next());
+
+					}
+
+					if(caixa.getSexo().equalsIgnoreCase("Masculino") || caixa.getSexo().equalsIgnoreCase("Feminino")){
+                    	controlador++;
+                    }
+
+				if(controlador==3){
+						System.out.println("\nDigite o Idade do caixa");
+						caixa.setIdade(scanner.nextInt());
+
+					}
+					if(caixa.getIdade()>0){
+						controlador++;
+					}
+
+				if(controlador==4){
+					System.out.println("\nDigite o ID do caixa");
+					caixa.setId(scanner.nextInt());
+
+				}
+					if(caixaDAO.buscarCaixa(caixa.getId())==null && caixaDAO.buscarCaixa(caixa.getId())==null && caixa.getId()!=0 && caixa.getId()>0){ 
+						controlador++;
+					}
+				
+					// ESTOU EDITANDO AQUI AINDA
+				if(controlador==5){
+					System.out.println("\nDigite o Login do caixa");
+					caixa.setLogin(scanner.next());
+	
+					}
+
+					//Criar uma Busca de Login No caixa e no gerente
+					if(gerenteDAO.buscarLoginGerente(caixa.getLogin())==null && caixaDAO.buscarLoginCaixa(caixa.getLogin())==null &&caixa.getLogin()!=null && caixa.getLogin()!=""){
+						controlador++;
+					}
+				if(controlador==6){
+					System.out.println("\nDigite o Senha do caixa");
+					caixa.setSenha(scanner.next());
+				}
+					if(caixa.getSenha()!=null && caixa.getSenha()!=""){
+						controlador++;
+						caixaDAO.cadastrar(caixa);
+					}
+
+				}
+
+				System.out.println("não foi possivel colocar os dados, insira novamente!");
+			}
 	}
 
-	public void alterar(String nomeDoCaixa){
 
-		CaixaVO caixaParaAlterar = new CaixaVO();
+	// ALTERAR E DELETAR Ã‰ USANDO O ID DO USUARIO QUE O CAIXA HERDA
+	public void alterar(int id){
 
-		CaixaDAO buscador = new CaixaDAO();
+		caixa = caixaDAO.buscarCaixa(id);
 
-		caixaParaAlterar = buscador.buscarCaixa(nomeDoCaixa);
+		if(caixaDAO.buscarCaixa(id)!=null){
+			System.out.println(caixa.toString());
 
-		// fazer o busca tipo do Caixa
-		System.out.println(caixaParaAlterar.toString());
+			controlador=0;
+			if(caixaDAO.buscarCaixa(id)!=null){
+
+				caixa = caixaDAO.buscarCaixa(id);
+	
+				System.out.println(caixa.toString());
+				controlador=0;
+				while(controlador!=7){
+	
+					if(controlador==0){
+						System.out.println("\nDigite o Salario do Caixa");
+						caixa.setSalario(scanner.nextFloat());
+	
+						if( caixa.getSalario()> 1100 && caixa.getSalario()!=0){
+							controlador++;
+						}
+					if(controlador==1){
+						System.out.println("\nDigite o Nome do caixa");
+						caixa.setNome(scanner.next());
+					}
+	
+						if(caixa.getNome() != null && caixa.getNome()!=""){
+						controlador ++;
+						}
+	
+					if(controlador==2){
+							System.out.println("\nDigite o Sexo do caixa");
+							caixa.setSexo(scanner.next());
+	
+						}
+	
+						if(caixa.getSexo().equalsIgnoreCase("Masculino") || caixa.getSexo().equalsIgnoreCase("Feminino")){
+							controlador++;
+						}
+	
+					if(controlador==3){
+							System.out.println("\nDigite o Idade do caixa");
+							caixa.setIdade(scanner.nextInt());
+	
+						}
+						if(caixa.getIdade()>0){
+							controlador++;
+						}
+	
+					if(controlador==4){
+						System.out.println("\nDigite o ID do caixa");
+						caixa.setId(scanner.nextInt());
+	
+					}
+						if(caixaDAO.buscarCaixa(caixa.getId())==null && caixaDAO.buscarCaixa(caixa.getId())==null && caixa.getId()!=0 && caixa.getId()>0){ 
+							controlador++;
+						}
+					
+					if(controlador==5){
+						System.out.println("\nDigite o Login do caixa");
+						caixa.setLogin(scanner.next());
 		
+						}
 
-		System.out.println("\nDigite o novo Nome para o Caixa: ");
-		caixaParaAlterar.setNome(scanner.next());
-		System.out.println("\nDigite a nova Idade para o Caixa: ");
-		caixaParaAlterar.setIdade(scanner.nextInt());
-		System.out.println("\nDigite o novo Sexo do Caixa: ");
-		caixaParaAlterar.setSexo(scanner.next());
-		System.out.println("\nDigite o novo Salario do Caixa: ");
-		caixaParaAlterar.setNome(scanner.next());
-
-		caixas = caixaDAO.buscaExcludente(nomeDoCaixa);
-
-		CaixaDAO.cadastrarAuxiliar(caixaParaAlterar);
-
-		for(int i=0;caixas.get(i)!=null;i++){
-
-			caixaDAO.cadastrar(caixas.get(i));
+						if(caixaDAO.buscarLoginCaixa(caixa.getLogin())==null && gerenteDAO.buscarLoginGerente(caixa.getLogin())==null &&caixa.getLogin()!=null && caixa.getLogin()!=""){
+							controlador++;
+						}
+					if(controlador==6){
+						System.out.println("\nDigite o Senha do caixa");
+						caixa.setSenha(scanner.next());
+					}
+						if(caixa.getSenha()!=null && caixa.getSenha()!=""){
+							controlador++;
+						}
+					}
+	
+					System.out.println("não foi possivel colocar os dados, insira novamente!");
+				}
+				caixas = caixaDAO.buscaExcludente(id);
+	
+				CaixaDAO.cadastrarAuxiliar(caixa);
+	
+				for(int i=0; caixas.get(i)!=null;i++){
+					caixaDAO.cadastrar(caixas.get(i));
+				}
+	
+			}
 		}
-		
 	}
 
-	public void deletar(String nomeDoCaixa){
+	public void deletar(int id){
 
-		caixas = caixaDAO.buscaExcludente(nomeDoCaixa);
+		caixas = caixaDAO.buscaExcludente(id);
 
 		
 		for(int i= 0;caixas.get(i)!=null;i++){
@@ -74,13 +210,6 @@ public class CaixaBO {
 					caixaDAO.cadastrar(caixas.get(i));
 				}
 			}
-
 		}
-	
-	// public void buscar (){ //talvez não seja necessário considerar depois
-	// 	//Buscar caixa baseado no nome do Caixa
-	// 	//Retornar Caixa;	
-	// }
+    
 }
-		
-
--- src/br/edu/ufersa/model/BO/UsuarioBO.java
+++ src/br/edu/ufersa/model/BO/UsuarioBO.java
@@ -1,32 +1,6 @@
-package mercadinhopl.model.BO;
+package br.edu.ufersa.model.BO;
 
-import mercadinhopl.model.VO.*;
-import mercadinhopl.model.DAO.*;
-import java.io.*;
-import java.util.*;
-
-public class UsuarioBO {
-
-	Scanner scanner = new Scanner(System.in);
-	UsuarioVO usuario = new UsuarioVO();
-	UsuarioDAO usuarioDAO = new UsuarioDAO();
-    
-    public void cadastrarUsuario(){
-		
-		System.out.println("Entre com um ID para o usuário: ");
-		usuario.setId(scanner.nextInt());
-		System.out.println("Entre com um login para o usuário: ");
-		usuario.setLogin(scanner.next());
-		System.out.println("Entre com uma senha para o usuário: ");
-		usuario.setSenha(scanner.next());
-
-		usuarioDAO.cadastrar(usuario);
-		usuarioDAO.listar();
-	}
-	public void alterar(UsuarioVO usuario){
-		//salvar alterações no BD
-	}
-	public void deletar(UsuarioVO usuario){
-		//Deletar do BD
-	}
+public abstract class UsuarioBO {
+   public abstract void cargo();
+   
 }
